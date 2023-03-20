6005bcdea4bc7415a08f4fbeade1a0719099a839
accuracytf <miltonhansson1@gmail.com>
2023-03-20 14:52:27 +0100

    Initial commit

---
+++ ProgProject/Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProgProject
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Player player;
+
+        int width, heigth;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            width = 1280;
+            heigth = 720;
+
+
+            _graphics.PreferredBackBufferHeight = heigth;
+            _graphics.PreferredBackBufferWidth = width;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player = new(Content.Load<Texture2D>("biran"), new Vector2(50,50));
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            player.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            player.Draw(_spriteBatch);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ProgProject/Player.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgProject
+{
+    internal class Player
+    {
+        
+        public Texture2D playerTexture;
+        public Vector2 playerPos;
+        public Vector2 velocity;
+        bool isGrounded;
+
+
+        public Player(Texture2D pTexture, Vector2 position)
+        {
+            this.playerTexture = pTexture;
+            this.playerPos = position;
+            isGrounded = false;
+
+        }
+        public void Update()
+        {
+            KeyboardState ks = Keyboard.GetState();
+
+            playerPos += velocity;
+            if (ks.IsKeyDown(Keys.A)){
+                velocity.X = -3f;
+            }
+            if (ks.IsKeyDown(Keys.D))
+            {
+                velocity.X = 3f;
+            }
+            if (ks.IsKeyDown(Keys.Space) && isGrounded)
+            {
+                playerPos.Y -= 10f;
+                velocity.Y = -5f;
+                isGrounded = false;
+            }
+            if (!isGrounded)
+            {
+                float i = 1;
+                velocity.Y += 0.15f * i;
+            }
+            
+            if (playerPos.Y + playerTexture.Height >= 720)
+            {
+                isGrounded = true;
+            }
+            if (isGrounded)
+            {
+                velocity.Y = 0f;
+            }
+
+
+
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(playerTexture, playerPos, Color.White);
+
+            // TODO: Add your drawing code here
+
+        }
+
+
+    }
+}
